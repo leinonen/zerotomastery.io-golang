b0e3e15f563cf07e0a72c74848bac2dad04185fd
Peter Leinonen <peter@leinonen.se>
2024-05-13 09:50:11 +0200

    exercise synchronization

--- exercise/sync/sync.go
+++ exercise/sync/sync.go
@@ -19,7 +19,57 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"os"
+	"strings"
+	"sync"
+	"unicode"
 )
 
-func main() {}
+type LetterCount struct {
+	count int
+	sync.Mutex
+}
+
+func analyze(str string, wg *sync.WaitGroup, counter *LetterCount) {
+	numLetters := 0
+	for _, char := range str {
+		if unicode.IsLetter(char) {
+			numLetters += 1
+		}
+	}
+	counter.Lock()
+	defer counter.Unlock()
+	defer wg.Done()
+	counter.count += numLetters
+}
+
+func main() {
+	var wg sync.WaitGroup
+	letterCount := LetterCount{}
+	rd := bufio.NewReader(os.Stdin)
+
+	for {
+		str, err := rd.ReadString('\n')
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			wg.Add(1)
+			go analyze(strings.TrimSpace(str), &wg, &letterCount)
+		}
+	}
+	wg.Wait()
+
+	letterCount.Lock()
+	count := letterCount.count
+	letterCount.Unlock()
+
+	fmt.Println("letter count =", count)
+}
